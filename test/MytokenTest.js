43112cb049d4c718bc5cb78e74dbb2903420fe3b
isk-bokang <bo.kang@iskra.world>
2022-05-19 09:50:24 +0900

    Add transaction test

--- test/MytokenTest.js
+++ test/MytokenTest.js
@@ -10,9 +10,26 @@ describe("Token Contract", function () {
     const myToken = await Token.deploy();
     const ownerBalance = await myToken.balanceOf(owner.address)
     
-    console.log(owner)
 
     expect(await myToken.totalSupply()).to.equal(ownerBalance);
 
+  });
+});
+
+describe("Transactions", function () {
+  it("Should tranfer tokens between accounts", async function () {
+    
+    const [owner, addr1, addr2] = await ethers.getSigners();
+
+    const Token = await ethers.getContractFactory("Mytoken")
+    const myToken = await Token.deploy();
+    
+    await myToken.transfer(addr1.address, 50);
+    expect(await myToken.balanceOf(addr1.address)).to.equal(50);
+
+    await myToken.transfer(addr2.address, 150);
+    expect(await myToken.balanceOf(addr2.address)).to.equal(150);
+
+    expect(await myToken.balanceOf(owner.address)).to.equal(await myToken.totalSupply() - 200);
   });
 });
